eb4b7504b1949c2790ab2abdc7216adb8678038e
Daryan Kamalifar <32463453+Daryan98@users.noreply.github.com>
2018-07-27 11:06:09 +0200

    using jsx and using nodemong and realod

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
@@ -12,3 +15,5 @@ app.use(require('./routes/speakers'));
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
+
+reload(server, app);
