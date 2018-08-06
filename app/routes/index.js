a0660260742f4213e19cb34bf37faa1c3e761396
Daryan Kamalifar <32463453+Daryan98@users.noreply.github.com>
2018-08-06 18:56:43 +0200

    starting again

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-
-  res.render('index');
-
+  res.send(`
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <h1>Welcome to Roux Meetups</h1>
+      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
+      <p>Roux Academy Meetups put together artists from all walks of life</p>
+      <script src="/reload/reload.js"></script>
+  `);
 });
 
 module.exports = router;
