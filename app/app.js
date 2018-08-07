819bf0b587471a0bcb4f305ab79617e2d21c41f8
Daryan Kamalifar <32463453+Daryan98@users.noreply.github.com>
2018-08-07 13:45:30 +0200

    adding more pages

--- app/app.js
+++ app/app.js
@@ -5,14 +5,16 @@ var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
-app.set("view engine", "ejs" );
-app.set("views", "app/views");
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
-app.locals.siteTitle = "Roux Meetups"
+app.locals.siteTitle = 'Roux Meetups';
+app.locals.allSpeakers = dataFile.speakers;
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
+app.use(require('./routes/feedback'));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
